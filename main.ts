78eba8e1add7b47c90d040ad7002d6e3c147ed24
Øystein Salomonsen <oystein.salomonsen@tekniskmuseum.no>
2021-11-19 22:06:47 +0100

    Oppdater pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,5 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (Program == 1) {
-        if (receivedNumber == 1 && radio.receivedPacket(RadioPacketProperty.SignalStrength) > -44.2) {
-            Kjør = true
-            grønnStart = input.runningTime()
-        }
-    } else if (Program == 2) {
-        if (receivedNumber == 0 && radio.receivedPacket(RadioPacketProperty.SignalStrength) > -44.2) {
-            Kjør = false
-        } else {
-            Kjør = true
-        }
-    }
+    RadioTallMottatt = receivedNumber
 })
 input.onButtonPressed(Button.A, function () {
     Program = 1
@@ -24,14 +13,7 @@ input.onButtonPressed(Button.B, function () {
     basic.pause(500)
     basic.clearScreen()
 })
-let grønnStart = 0
-let Program = 0
-let Kjør = false
-radio.setGroup(1)
-Kjør = false
-Program = 0
-let kjøretidProgA = 5000
-basic.forever(function () {
+function BitBit_bil () {
     if (Kjør) {
         if (bitbot.readLine(BBLineSensor.Left) == 1) {
             bitbot.rotate(BBRobotDirection.Left, 15)
@@ -46,4 +28,34 @@ basic.forever(function () {
     if (Program == 1 && input.runningTime() > grønnStart + kjøretidProgA) {
         bitbot.go(BBDirection.Forward, 0)
     }
+}
+function Program_bil () {
+    if (Program == 1) {
+        bitbot.setLedColor(0xFF0000)
+        if (RadioTallMottatt == 1 && radio.receivedPacket(RadioPacketProperty.SignalStrength) > -44.2) {
+            Kjør = true
+            grønnStart = input.runningTime()
+        }
+    } else if (Program == 2) {
+        bitbot.setLedColor(0x0000FF)
+        if (RadioTallMottatt == 0 && radio.receivedPacket(RadioPacketProperty.SignalStrength) > -44.2) {
+            Kjør = false
+        } else {
+            Kjør = true
+        }
+    }
+}
+let grønnStart = 0
+let RadioTallMottatt = 0
+let kjøretidProgA = 0
+let Program = 0
+let Kjør = false
+radio.setGroup(1)
+Kjør = false
+Program = 0
+kjøretidProgA = 5000
+RadioTallMottatt = 0
+basic.forever(function () {
+    Program_bil()
+    BitBit_bil()
 })
